3afe3e48209a288fe37aa304cc492aa5bb327a1f
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-09-29 19:53:17 +0300

    Update Typing speed.js
    Improved the runTime and endGame functions. I also corelated the endGame function with the changes occurring inside the pop-up.

--- Typing speed.js
+++ Typing speed.js
@@ -1,7 +1,8 @@
-let second = 60;
+let second = 60, wpm = 0, record = 0, accuracy = 100;
 let timer;
 let start = false;
 
+//The game starts when the first character was written in the user text box.
 function startGame() {
     if (!start) {
         timer = setInterval(runTime, 1000);
@@ -9,13 +10,35 @@ function startGame() {
     }
 }
 
+//The timer count down from 60 to 0. If the minute elapsed, the game ends.
 function runTime() {
     --second;
-    document.querySelector(".time").innerText = "00:" + second;
+    let prefSec = '';
+    if (second < 10) {
+        prefSec = '0';
+    }
+    document.querySelector(".time").innerText = "00:" + prefSec + second;
+    if (second === 0) {
+        endGame();
+    }
 }
 
+//The game is restarted
 function restart () {
     window.location.reload();
+}
+
+/*The game ends and the pop-up occurs displaying the statistics of the game
+ finished and the button to start a new game*/
+function endGame() {
+    clearInterval(timer);
+    if (wpm > record) {
+        record = wpm;
+    }
+    document.getElementsByClassName("wpm")[0].innerText = "WPM: " + wpm;
+    document.getElementsByClassName("record")[0].innerText = "RECORD: " + record + " WPM";
+    document.getElementsByClassName("accuracy")[0].innerText = "ACCURACY: " + accuracy + "%";
+    document.getElementsByClassName("pop-up")[0].style.visibility = "visible";
 }
 
 document.getElementsByClassName("newText")[0].addEventListener("keydown", startGame);
