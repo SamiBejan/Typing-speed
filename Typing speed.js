c089f1f3124c9eaaf7ed94fc739831c250fe0f20
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-10-09 14:44:22 +0300

    Update Typing speed.js
    Displayed the model text as an array of "span" elements and also implemented the functionality to check the written characters vs the characters in the model text.

--- Typing speed.js
+++ Typing speed.js
@@ -1,6 +1,27 @@
-let second = 60, wpm = 0, record = 0, accuracy = 100;
+let modelText = document.querySelector(".modelText");
+let userText = document.querySelector(".userText");
+let textArray = new Array(1214);
+let second = 60, wordsCnt = 0, record = 0, accuracy = 0, prevInd = 0;
 let timer;
 let start = false;
+
+addText();
+
+//The model text is formatted as an array of span elements and displayed in the upper box //
+function addText() {
+    let text = "I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches history, and my mother is a nurse at a big hospital. " + 
+    "My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. " +
+    "She has grown old, but she is still very strong. She cooks the best food! My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. " +
+    "My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much. " +
+    "My favorite beach is called Emerson Beach. It is very long, with soft sand and palm trees. It is very beautiful. I like to make sandcastles and watch the sailboats go by. " +
+    "Sometimes there are dolphins and whales in the water! Every morning we look for shells in the sand. I found fifteen big shells last year. I put them in a special place in my room. " +
+    "This year I want to learn to surf. It is hard to surf, but so much fun! My sister is a good surfer. She says that she can teach me. I hope I can do it!"
+    for (let i = 0; i < textArray.length; ++i) {
+        textArray[i] = document.createElement("span");
+        textArray[i].innerText = text[i];
+        modelText.appendChild(textArray[i]);
+    }
+}
 
 //The game starts when the first character was written in the user text box.
 function startGame() {
@@ -10,7 +31,7 @@ function startGame() {
     }
 }
 
-//The timer count down from 60 to 0. If the minute elapsed, the game ends.
+//The timer counts down from 60 to 0. If the minute elapsed, the game ends.
 function runTime() {
     --second;
     let prefSec = '';
@@ -23,6 +44,25 @@ function runTime() {
     }
 }
 
+/*This function checks the correctness of the text wrote by the user by comparing each new written character with 
+the character located on the same index in the model text. If they are the same, the character from the model text 
+becomes green. If not, it becomes red. If a character is erased from the written text, the previously coloured
+character from the model text located on the right side of the current cursor position becomes black*/
+function checkLetter() {
+    let ind = userText.innerText.length - 1;
+    if (prevInd > ind) {
+        textArray[prevInd].classList.remove("wrong");
+        textArray[prevInd].classList.remove("correct"); 
+    }
+    if (ind >= 0 && userText.innerText[ind] != textArray[ind].innerText) {
+        textArray[ind].classList.remove("correct"); 
+        textArray[ind].classList.add("wrong"); 
+    } else if (ind >= 0 && userText.innerText[ind] == textArray[ind].innerText) {
+        textArray[ind].classList.add("correct"); 
+    }
+    prevInd = ind;
+}
+
 //The game is restarted
 function restart () {
     window.location.reload();
@@ -32,13 +72,14 @@ function restart () {
  finished and the button to start a new game*/
 function endGame() {
     clearInterval(timer);
-    if (wpm > record) {
-        record = wpm;
+    if (wordsCnt > record) {
+        record = wordsCnt;
     }
-    document.getElementsByClassName("wpm")[0].innerText = "WPM: " + wpm;
+    document.getElementsByClassName("wpm")[0].innerText = "WPM: " + wordsCnt;
     document.getElementsByClassName("record")[0].innerText = "RECORD: " + record + " WPM";
     document.getElementsByClassName("accuracy")[0].innerText = "ACCURACY: " + accuracy + "%";
     document.getElementsByClassName("pop-up")[0].style.visibility = "visible";
 }
 
-document.getElementsByClassName("newText")[0].addEventListener("keydown", startGame);
+userText.addEventListener("keydown", startGame);
+userText.addEventListener("input", checkLetter);
