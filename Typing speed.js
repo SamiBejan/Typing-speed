9f0f550e729ea147864b5cc109fc9d1aeced235a
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-11-07 13:39:21 +0200

    Update Typing speed.js
    Added new functionalities to the project:
    - implemented the "checkWord()" function which checks the correctness of each new written word, but also counts the number of written and correct words.
    - implemented a function that gives a certain background to each new written word.
    - created an array storing the positions delimiting the words;
    - removed the feature storing the record of written words, because if I wanted to continue using it, I should've replaced the operation "window.location.reload()" within the "restart()" function with a more extended code;

--- Typing speed.js
+++ Typing speed.js
@@ -1,15 +1,15 @@
 let modelText = document.querySelector(".modelText");
 let userText = document.querySelector(".userText");
-let textArray = new Array(1214);
-let second = 60, wordsCnt = 0, record = 0, accuracy = 0, prevInd = 0;
-let timer;
+let textArray = new Array(1214), wordDelimiter = [-1];
+let second = 60, wordsCnt = 0, correctWords = 0, ind = 0, prevInd = 0;
+let timer, text;
 let start = false;
 
 addText();
 
 //The model text is formatted as an array of span elements and displayed in the upper box //
 function addText() {
-    let text = "I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches history, and my mother is a nurse at a big hospital. " + 
+    text = "I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches history, and my mother is a nurse at a big hospital. " + 
     "My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. " +
     "She has grown old, but she is still very strong. She cooks the best food! My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. " +
     "My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much. " +
@@ -20,7 +20,11 @@ function addText() {
         textArray[i] = document.createElement("span");
         textArray[i].innerText = text[i];
         modelText.appendChild(textArray[i]);
+        if (/\s/.test(text[i])) {
+            wordDelimiter.push(i);
+        }
     }
+    wordDelimiter.push(textArray.length);
 }
 
 //The game starts when the first character was written in the user text box.
@@ -46,21 +50,58 @@ function runTime() {
 
 /*This function checks the correctness of the text wrote by the user by comparing each new written character with 
 the character located on the same index in the model text. If they are the same, the character from the model text 
-becomes green. If not, it becomes red. If a character is erased from the written text, the previously coloured
+becomes green. If not, it becomes red. If a character is erased from the written text, the already coloured
 character from the model text located on the right side of the current cursor position becomes black*/
 function checkLetter() {
-    let ind = userText.innerText.length - 1;
-    if (prevInd > ind) {
+    ind = userText.innerText.length - 1;
+    if (prevInd > ind && ind <= text.length - 1) {
         textArray[prevInd].classList.remove("wrong");
         textArray[prevInd].classList.remove("correct"); 
     }
-    if (ind >= 0 && userText.innerText[ind] != textArray[ind].innerText) {
+    if (ind >= 0 && ind <= text.length - 1 && userText.innerText[ind] != textArray[ind].innerText) {
         textArray[ind].classList.remove("correct"); 
         textArray[ind].classList.add("wrong"); 
-    } else if (ind >= 0 && userText.innerText[ind] == textArray[ind].innerText) {
+    } else if (ind >= 0 && ind <= text.length - 1 && userText.innerText[ind] == textArray[ind].innerText) {
         textArray[ind].classList.add("correct"); 
     }
     prevInd = ind;
+}
+
+/*This function compares each new written word with the correspondent word from the model text. 
+If the written word is correct, it will receive a green background. If it's wrong, it will receive a red background.
+If the user erases characters from a word, the background of that word disappears. The function also
+counts the written words and the correct words*/
+function checkWord() {
+    let colour = " ", isCorrect = true;
+    if (wordDelimiter[wordsCnt + 1] === ind + 1) {
+        ++wordsCnt;
+        if (userText.innerText.substring(wordDelimiter[wordsCnt - 1], wordDelimiter[wordsCnt]) == text.substring(wordDelimiter[wordsCnt - 1], wordDelimiter[wordsCnt])) {
+            colour = "#d6f0b6";
+            ++correctWords;
+        } else {
+            colour = "#fac8c8";
+            isCorrect = false;
+            if (correctWords === wordsCnt) {
+                --correctWords;
+            }
+        }
+        colourWord(colour);
+    } else if (wordDelimiter[wordsCnt] > ind + 1) {
+        colour = "#00000000";
+        colourWord(colour);
+        --wordsCnt;
+        if (!isCorrect) {
+            --correctWords;
+        }
+    }
+}
+
+/*Each new written word from the model text receives a background. 
+If characters from that word are erased in the user text box, the background disappears*/
+function colourWord(colour) {
+    for (let i = wordDelimiter[wordsCnt - 1] + 1; i <= wordDelimiter[wordsCnt] - 1; ++i) {
+        textArray[i].style.background = colour;
+    }
 }
 
 //The game is restarted
@@ -72,14 +113,12 @@ function restart () {
  finished and the button to start a new game*/
 function endGame() {
     clearInterval(timer);
-    if (wordsCnt > record) {
-        record = wordsCnt;
-    }
     document.getElementsByClassName("wpm")[0].innerText = "WPM: " + wordsCnt;
-    document.getElementsByClassName("record")[0].innerText = "RECORD: " + record + " WPM";
-    document.getElementsByClassName("accuracy")[0].innerText = "ACCURACY: " + accuracy + "%";
+    document.getElementsByClassName("accuracy")[0].innerText = "ACCURACY: " +  parseInt(correctWords / wordsCnt * 100) + "%";
     document.getElementsByClassName("pop-up")[0].style.visibility = "visible";
 }
 
+userText.addEventListener("contextmenu", event => event.preventDefault());
 userText.addEventListener("keydown", startGame);
 userText.addEventListener("input", checkLetter);
+userText.addEventListener("input", checkWord);
